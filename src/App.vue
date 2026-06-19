8df44f86b5a3a8d3bba381950ba92c7d5e2ab000
NicholasOyafuso <nicholas.oyafuso@gmail.com>
2026-06-19 00:49:27 -0300

    Add files via upload
    Signed-off-by: NicholasOyafuso <nicholas.oyafuso@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { ref, provide } from 'vue'
+
+const cartItems = ref([])
+
+function addToCart(produto) {
+  const existente = cartItems.value.find(item => item.titulo === produto.titulo)
+
+  if (existente) {
+    existente.quantidade++
+  } else {
+    cartItems.value.push({
+      id: Date.now(),
+      titulo: produto.titulo,
+      descricao: produto.descricao,
+      imagem: produto.imagem,
+      valor: produto.valor,
+      quantidade: 1
+    })
+  }
+}
+
+function updateQuantity(id, novaQuantidade) {
+  if (novaQuantidade <= 0) {
+    removeItem(id)
+    return
+  }
+  const item = cartItems.value.find(i => i.id === id)
+  if (item) item.quantidade = novaQuantidade
+}
+
+function removeItem(id) {
+  cartItems.value = cartItems.value.filter(i => i.id !== id)
+}
+
+provide('cart', { cartItems, addToCart, updateQuantity, removeItem })
+</script>
+
 <template>
   <router-view />
 </template>
